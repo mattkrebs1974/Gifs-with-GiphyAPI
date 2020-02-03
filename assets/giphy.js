e3b8ccfde4d86c39652a7ea6922b56efa09fffb0
mattkrebs1974 <mattkrebsemail@gmail.com>
2020-02-03 12:37:59 -0500

    pause/start works

--- assets/giphy.js
+++ assets/giphy.js
@@ -1,16 +1,89 @@
 $(document).ready(function () {
 
+    var interests = ["Baseball", "Basketball", "Football", "Hockey"];
+
+function renderButtons() {
+
+    $("#buttons").empty();
+
+
+    // for (var i=0; i<interests.length; i++) {
+
+    //in place of for loop "$.each (function) -- idea given to me by "Aja" 
+
+$.each(interests, function(index,value) {
+
+        var a =$("<button>");
+        a.addClass("buttons");
+        a.attr("data-a", value);
+        a.text(value);
+        $("#buttons").append(a);
+
+
+
+    });
+}
 
 
 
 
-$("button").on("click", function () {
-    // Grabbing and storing the data-sports property value from the button
-    var sports = $(this).attr("data-a");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+    $(document).on("click","#submit", function (event) {
+
+        event.preventDefault();
+
+        var newInterest = $("#topic").val().trim();
+
+        interests.push(newInterest);
+
+        renderButtons();
+
+    });
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).on("click", ".buttons", function () {
+    
+
+    var sports = $(this).attr("data-a").trim();
 
     // Constructing a queryURL using the sports name
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        sports + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=4";
+        sports + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
 
     // Performing an AJAX request with the queryURL
     $.ajax({
@@ -19,30 +92,87 @@ $("button").on("click", function () {
     })
         // After data comes back from the request
         .then(function (response) {
-            console.log("queryURL " + queryURL);
-
-            console.log("response " + response);
+          
             // storing the data from the AJAX request in the results variable
             var results = response.data;
 
-            console.log("response.data " + response.data)
+           
+
+            $("#gifs-appear-here").empty();
+
 
             // Looping through each result item
-            for (var i = 0; i < results.length; i++) {
+            $.each(results, function (index, value)  {
+
+                var getDiv = $("#gifs-appear-here");
 
                 // Creating and storing a div tag
                 var sportsDiv = $("<div>");
 
                 // Creating a paragraph tag with the result item's rating
+
+                var p = $("<p>").text("Gif Rating: " + results[index].rating);
                
             sportsDiv.addClass("carousel-item");
+            
+            
+            
+            sportsDiv.attr("number",index);
+            sportsDiv.addClass("active");
+
+         
+
+            // console.log("number"+number);
+
+            // function newCarousel() {
+            //     if (index=0){
+
+            //         sportsDiv.addClass("active");
+            //     }
+            //     else{
+
+
+            //     }
+
+
+            // }
+            // newCarousel();
+            //     console.log(index);
+          
+
+          
+
+
+            // sportsDiv.attr("slide",i);
+
+
+//             var numberOfSlide=sportsDiv.slide.val();
+
+// console.log("numberOfSlide"+numberOfSlide);
+
+           // why won't this work?
+
+
+                
+
+
+
+          
+                
+
+                
+            
+
+            // var indexShown
+            // based on indexSHown add active class and remove active class from everything else. 
+            
 
             // if (i=0) {
             //     sportsDiv.addClass("active");
 
             // }
 
-
+// store the index of the image as a data attribute - when you are showing the image grab the value add class (if else)
            
          
             //    sportsDiv.addClass("d-block");
@@ -64,16 +194,34 @@ $("button").on("click", function () {
                 // Setting the src attribute of the image to a property pulled off the result item
                 // sportsImage.attr("id", sports+results[i]);
 
-                sportsImage.attr("src", results[i].images.original.url);
+                sportsImage.attr("src", results[index].images.original.url);
+
+
+                sportsImage.attr("data-still", results[index].images.fixed_height_still.url);
+
+                sportsImage.attr("data-animate", results[index].images.fixed_height.url);
+
+                sportsImage.attr("data-state", "still");
+
+                sportsImage.addClass("gif");
+
+                sportsDiv.append(p);
+
+
+
 
                 // Appending the paragraph and image tag to the sportsDiv
-              console.log(sportsImage);
+              
+
+
 
 
                 // sportsDiv.append(pp);
 
 
                 sportsDiv.append(sportsImage);
+
+                getDiv.prepend(sportsDiv);
               
 
                 
@@ -97,11 +245,49 @@ $("button").on("click", function () {
 
 
 
-                $("#gifs-appear-here").prepend(sportsDiv);
+               
+
+               
 
                 $("#photoModal").modal("show");
-            }
+            });
         });
-});
+
+    });
+renderButtons();
+    // function nextSlide() {
+
+    //     if (slide > -1) {
+
+    //         $("#mycarousel").carousel({ interval: 1000 });
+    //     } else {
+    //         $("#mycarousel").carousel({ interval: 5 });
+
+    //     }
+
+    // }
+
+
+
+
+    $(document).on("click", ".gif", function () {
+        var state = $(this).attr("data-state");
+
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    });
+
+
+
 
 });
+
+
+    
+
+
